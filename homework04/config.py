53bb7575b74a41c270e5ab3c1ae60a4c7eb47db6
AndreiGlyzin <andrei.olofffmag@gmail.com>
2019-12-16 23:32:42 +0300

    Исправлены ошибки

--- homework04/config.py
+++ homework04/config.py
@@ -1,6 +1,6 @@
 VK_CONFIG = {
     'domain': 'https://api.vk.com/method',
-    'access_token': '38bbc44c6fb7e8ce15de4436fadf2e1cb6c8f278a4bb92f933ff7734cd5751f04e099aacd5e8b9da4f5e9',
+    'access_token': '---',
     'version': '5.87',
     'id': 188286293
 }
